59fedf34d63e98ee1caf3226d8b2e355d4ee2ba9
jaime <jandres.vr@hotmail.com>
2020-03-19 14:33:53 -0500

    feature: changes in the nav and form

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,20 +1,23 @@
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
 import "./header.css";
-import logo from "../img/logo3.png";
+
 function Header() {
   return (
-    <Navbar  expand="lg" fixed="top" className="navbar">
-      <Navbar.Brand href="#home"><img id="logo" src={logo} alt="logo" /></Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto"></Nav>
-        <Nav>
-          <Nav.Link href="#home" className="links">Home</Nav.Link>
-          <Nav.Link href="#about" className="links">About</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <section className="side">
+      <input type="checkbox" id="check"/>
+      <label for="check">
+        <i className="fas fa-bars" id="open"></i>
+        <i className="fas fa-times" id="close"></i>
+      </label>
+      <div className="sidebar">
+
+        <header>App</header>
+        <ul>
+          <li><a href="#home"><i className="fas fa-qrcode"></i>Home</a></li>
+          <li><a href="#about"><i className="fas fa-address-book"></i>Team</a></li>
+        </ul>
+      </div>
+    </section>
   );
 }
 
